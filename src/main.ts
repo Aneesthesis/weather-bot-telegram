6f2718e4333b49ec3b2763da879e536107d32b4a
aneesthesis <trulyanees@gmail.com>
2023-11-27 12:46:37 +0530

    v.1.2 backend live on render

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,29 @@
 // main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ImATeapotException } from '@nestjs/common/exceptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const whitelist = [
-    'https://aneesweatherbot.onrender.com/',
-    'http://localhost:3000',
-  ];
+  const whitelist = 'https://aneesweatherbot.onrender.com/';
 
   app.enableCors({
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
     origin: function (origin, callback) {
-      if (!origin || whitelist.includes(origin)) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (whitelist.includes(origin)) {
         callback(null, true);
       } else {
-        console.log('Not allowed');
-        callback(new Error('Not allowed by CORS'), false);
+        console.log('not alowwed');
+        callback(new ImATeapotException('not allowed'), false);
       }
     },
   });
-  // app.enableCors({
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   origin: function (origin, callback) {
-  //     if (!origin) {
-  //       callback(null, true);
-  //       return;
-  //     }
-  //     if (whitelist.includes(origin)) {
-  //       callback(null, true);
-  //     } else {
-  //       console.log('not allowed');
-  //       callback(new ImATeapotException('not allowed'), false);
-  //     }
-  //   },
-  // });
 
   await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
