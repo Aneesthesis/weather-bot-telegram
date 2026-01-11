90ad59b160657567a2a7f0b4644c97daff1659a9
Anees <trulyanees@gmail.com>
2026-01-11 21:58:31 +0530

    feat: add dotenv and config module for environment variable management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import {
   MiddlewareConsumer,
   RequestMethod,
 } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TelegramModule } from './telegram/telegram.module';
 import { AdminModule } from './admin/admin.module';
 import { DatabaseModule } from './database/database.module';
@@ -12,7 +13,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TelegramModule, AdminModule, DatabaseModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TelegramModule,
+    AdminModule,
+    DatabaseModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 import { User, UserSchema } from '../user/user.model';
 import { Admin, AdminSchema } from '../admin/admin.model';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: async () => ({
-        uri: 'mongodb+srv://crunchysambusa:jmGZjwgBdC4gcBlZ@cluster0.6l6nxek.mongodb.net/Weather_bot_admin?retryWrites=true&w=majority',
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
       }),
+      inject: [ConfigService],
     }),
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
